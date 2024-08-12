f18a971af7e2d793426458fcd516425be9c1c17c
PC1237 <simovanev@abv.bg>
2024-08-12 12:18:00 +0300

    Get single employee.

--- src/main/java/com/spring/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/spring/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ package com.spring.cruddemo.rest;
 import com.spring.cruddemo.entity.Employee;
 import com.spring.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,14 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee= employeeService.findById(id);
+        if (employee== null){
+            throw new RuntimeException("Employee not found - "+id);
+        }
+        return  employee;
     }
 }
