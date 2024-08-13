8e46565cb290071a3eee477e0d03bf6192492e63
Simovanev <simovanev@abv.bg>
2024-08-13 06:17:45 +0300

    Implement Spring Data JPA.

--- src/main/java/com/spring/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/spring/cruddemo/service/EmployeeServiceImpl.java
@@ -1,39 +1,43 @@
 package com.spring.cruddemo.service;
 
-import com.spring.cruddemo.dao.EmployeeDAO;
 import com.spring.cruddemo.entity.Employee;
+import com.spring.cruddemo.repository.EmployeeRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> byId = employeeRepository.findById(id);
+        if (byId.isPresent()) {
+            return byId.get();
+        } else throw new RuntimeException("Employee not found");
     }
 
     @Override
-    @Transactional
+
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
+
     public void delete(int id) {
-        employeeDAO.delete(id);
+        employeeRepository.deleteById(id);
     }
 }
