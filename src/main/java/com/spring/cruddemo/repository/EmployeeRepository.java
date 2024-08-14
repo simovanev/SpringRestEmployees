32c4e138f4931bb310cbe9089770cd02ca9adffd
Simovanev <simovanev@abv.bg>
2024-08-14 06:07:39 +0300

    Implement Spring Security. Property for username and password.

--- src/main/java/com/spring/cruddemo/repository/EmployeeRepository.java
+++ src/main/java/com/spring/cruddemo/repository/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.spring.cruddemo.repository;
 
 import com.spring.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "members")
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
