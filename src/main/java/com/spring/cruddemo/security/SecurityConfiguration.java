a43eb120dc6d06f88d34ee70eb504974e9244a45
PC1237 <simovanev@abv.bg>
2024-08-16 12:36:00 +0300

    Add Security Configuration Class with passwords {noop} and roles

---
+++ src/main/java/com/spring/cruddemo/security/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.spring.cruddemo.security;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john= User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails marry= User.builder()
+                .username("marry")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        UserDetails susan= User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john,marry,susan);
+    }
+}
