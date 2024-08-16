83cf11c28cdd29c2a0a98d7423b55fb5fa03361e
PC1237 <simovanev@abv.bg>
2024-08-16 13:03:58 +0300

    Fix

---
+++ src/main/java/com/spring/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.spring.cruddemo.service;
+
+import com.spring.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void delete(int id);
+}
+
---
+++ src/main/java/com/spring/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,43 @@
+package com.spring.cruddemo.service;
+
+import com.spring.cruddemo.entity.Employee;
+import com.spring.cruddemo.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> byId = employeeRepository.findById(id);
+        if (byId.isPresent()) {
+            return byId.get();
+        } else throw new RuntimeException("Employee not found");
+    }
+
+    @Override
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+
+    public void delete(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
