b1ef25c89bdde4f51e860a3d2748f4002ab6e48b
Simovanev <simovanev@abv.bg>
2024-08-19 06:24:38 +0300

    Configure to use information from the database for authentication.

--- src/main/java/com/spring/cruddemo/security/SecurityConfiguration.java
+++ src/main/java/com/spring/cruddemo/security/SecurityConfiguration.java
@@ -10,33 +10,42 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails john= User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails marry= User.builder()
-                .username("marry")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails susan= User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,marry,susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails john= User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails marry= User.builder()
+//                .username("marry")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails susan= User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,marry,susan);
+//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(config->config
@@ -47,7 +56,7 @@ public class SecurityConfiguration {
                 .requestMatchers(HttpMethod.DELETE,"/api/employees").hasRole("ADMIN"));
 
         http.httpBasic(Customizer.withDefaults());
-        http.csrf(c->c.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
