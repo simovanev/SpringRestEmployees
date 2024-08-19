9f84ac9cc21e61b27480f2ed2cb230158ec3944f
Simovanev <simovanev@abv.bg>
2024-08-19 05:58:13 +0300

    Implement Spring Security. Request Authorisation

--- src/main/java/com/spring/cruddemo/security/SecurityConfiguration.java
+++ src/main/java/com/spring/cruddemo/security/SecurityConfiguration.java
@@ -3,9 +3,14 @@ package com.spring.cruddemo.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -31,5 +36,18 @@ public class SecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(john,marry,susan);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(config->config
+                .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE,"/api/employees").hasRole("ADMIN"));
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(c->c.disable());
+        return http.build();
     }
 }
